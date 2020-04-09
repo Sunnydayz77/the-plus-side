b87810b42acfad5868b0b022bab10b4c0a9eda22
JenPiwonskiGA <jenpiwonski@gmail.com>
2020-04-08 20:12:03 -0400

    comments model created and all new models migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_164159) do
+ActiveRecord::Schema.define(version: 2020_04_08_191055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blog_post_comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "blog_post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["blog_post_id"], name: "index_blog_post_comments_on_blog_post_id"
+    t.index ["user_id"], name: "index_blog_post_comments_on_user_id"
+  end
+
+  create_table "blog_posts", force: :cascade do |t|
+    t.string "image_url"
+    t.string "title"
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_blog_posts_on_user_id"
+  end
+
+  create_table "news_article_comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.bigint "news_article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["news_article_id"], name: "index_news_article_comments_on_news_article_id"
+    t.index ["user_id"], name: "index_news_article_comments_on_user_id"
+  end
+
+  create_table "news_articles", force: :cascade do |t|
+    t.string "image_url"
+    t.string "title"
+    t.string "article_url"
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_news_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -26,4 +67,10 @@ ActiveRecord::Schema.define(version: 2020_04_08_164159) do
     t.string "description"
   end
 
+  add_foreign_key "blog_post_comments", "blog_posts"
+  add_foreign_key "blog_post_comments", "users"
+  add_foreign_key "blog_posts", "users"
+  add_foreign_key "news_article_comments", "news_articles"
+  add_foreign_key "news_article_comments", "users"
+  add_foreign_key "news_articles", "users"
 end
