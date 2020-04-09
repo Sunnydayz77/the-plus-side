a390d95b17336e46f5d69c2d089386afc367ea60
JenPiwonskiGA <jenpiwonski@gmail.com>
2020-04-09 15:10:28 -0400

    controller fix

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,6 +1,6 @@
 class BlogPostsController < ApplicationController
-  before_action :set_blog_post, only: [:show, :update, :destroy]
-  before_action :authorize_request, only: [:create, :update, :destroy]
+  before_action :set_blog_post, only: [:show, :update, :destroy, :add_blog_post_comment]
+  before_action :authorize_request, only: [:create, :update, :destroy, :add_blog_post_comment]
 
   # GET /blog_posts
   def index
