4318faa5d5d77a5fea2113656c88709e6e5646f7
JenPiwonskiGA <jenpiwonski@gmail.com>
2020-04-12 16:15:35 -0400

    added deployment links to api-helper and application_controller

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,8 +1,6 @@
 const axios = require('axios');
 
-const baseUrl = 'https://the-plus-side.herokuapp.com/';
-
-// const baseUrl = process.env.NODE_ENV === 'production' ? 'https://the-plus-side.herokuapp.com/' : 'http://localhost:3000'
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://the-plus-side.herokuapp.com/' : 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl 
