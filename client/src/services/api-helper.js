f5ff3b958d97e31b80192e6ee49060b1b8f05598
JenPiwonskiGA <jenpiwonski@gmail.com>
2020-04-12 15:29:33 -0400

    api-helper update

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://the-plus-side.herokuapp.com/' : 'http://localhost:3000'
+const baseUrl = 'https://the-plus-side.herokuapp.com/';
+
+// const baseUrl = process.env.NODE_ENV === 'production' ? 'https://the-plus-side.herokuapp.com/' : 'http://localhost:3000'
 
 const api = axios.create({
   baseURL: baseUrl 
